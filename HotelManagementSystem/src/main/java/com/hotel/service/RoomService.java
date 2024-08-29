df9771f0d7c7c7af0b6975d4f1fc55c97675fad1
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-08-29 12:30:00 +0300

    Update RoomService.java

--- HotelManagementSystem/src/main/java/com/hotel/service/RoomService.java
+++ HotelManagementSystem/src/main/java/com/hotel/service/RoomService.java
@@ -1,5 +1,18 @@
 package com.hotel.service;
 
+import com.hotel.domain.Room;
+import com.hotel.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RoomService {
 
+    @Autowired
+    private RoomRepository repository ;
+
+    //save a room
+    public void saveRoom(Room room) {
+        repository.save(room);
+    }
 }
