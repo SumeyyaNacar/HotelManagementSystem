9f7a64f78fcbaefa2d6b9d0315b3ce116dd09a88
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-09-04 20:34:06 +0300

    Update Service.java

--- HotelManagementSystem/src/main/java/com/hotel/service/GuestService.java
+++ HotelManagementSystem/src/main/java/com/hotel/service/GuestService.java
@@ -1,5 +1,31 @@
 package com.hotel.service;
 
+import com.hotel.domain.Guest;
+import com.hotel.exception.GuestNotFoundException;
+import com.hotel.repository.GuestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class GuestService {
 
+    private final GuestRepository guestRepository;
+
+    //save a guest
+    public void saveGuest(Guest guest) {
+        guestRepository.save(guest);
+    }
+
+    //get all guests
+    public List<Guest> getAllGuest() {
+        return guestRepository.findAll();
+    }
+
+    //get guest by id
+    public Guest getGuestById(Long id) {
+       return guestRepository.findById(id).orElseThrow(()->new GuestNotFoundException("Guest is not found by id : "+ id));
+    }
 }
