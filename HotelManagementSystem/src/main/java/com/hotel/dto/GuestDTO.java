fc5c2944a3c12728ce0025bc40f207c4bd069e63
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-09-04 20:33:28 +0300

    Create GuestDTO.java

---
+++ HotelManagementSystem/src/main/java/com/hotel/dto/GuestDTO.java
@@ -0,0 +1,27 @@
+package com.hotel.dto;
+
+import com.hotel.domain.Reservation;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Getter
+@Setter
+public class GuestDTO {
+
+    @NotBlank(message = "Lutfen gecerli bir deger giriniz!")
+    private String name;
+
+    //contact inf
+    @NotBlank(message = "Lutfen gecerli bir deger giriniz!")
+    private String tcNo;
+
+    @NotBlank(message = "Lutfen gecerli bir deger giriniz!")
+    private String medeniDurum;
+
+    //private List<Reservation> reservation;
+
+
+}
